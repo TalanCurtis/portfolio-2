f8b741561df8e4988fe21d6b1af5e173d92de028
Alan Curtis <acurtis@badgersport.local>
2019-01-07 15:05:56 -0700

    added icons for skills and content json.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import content from '../../content.json';
 
 const Skills = () => {
+  let programming = content.skills.programming.cards.map((x,i)=>{
+    return (
+            <div key={i} className="SkillCard">
+                <img src={`/images/icons/${x.image}`} alt={x.image}/>
+                <h4>{x.title}</h4>
+            </div>
+    )
+  })
   return (
     <div className = "Skills">
-      Skills comp
+      <div className="title">
+        <div className="h2">Skills</div>
+        <hr/>
+      </div>
+      <div className="skill-section">
+        <div className="sub-header">
+          <div className="h3">Programming</div>
+          <hr style={{width:"98%"}}/>
+        </div>
+        {programming}
+      </div>
     </div>
   )
 }
