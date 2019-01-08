703f75ccf17dd13aed73085df4692e30cf46c6e8
Alan Curtis <acurtis@badgersport.local>
2019-01-08 13:40:46 -0700

    dummy content for projects and added portfolio section.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import content from '../../content.json';
 
 const Portfolio = () => {
+
+    let projects = content.projects.map((x,i)=>{
+      return (
+              <div key={i} className="project-card">
+                  <img src={`/images/icons/${x.image}`}  style={{width:"60px"}}alt={x.image}/>
+                  <div className="h4">{x.title}</div>
+              </div>
+      )
+    })  
+    
   return (
     <div className = "Portfolio">
-      Portfolio comp
+      <div className="title">
+        <div className="h2">Portfolio</div>
+        <hr style={{width:"98%"}}/>
+      </div>
+      <div className="project-container">
+        {projects}
+      </div>
     </div>
   )
 }
