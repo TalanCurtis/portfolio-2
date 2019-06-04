8cfb2e31e7a544c13763a7ee7a1eb49470a1b3df
Alan Curtis <acurtis@badgersport.local>
2019-06-03 22:23:02 -0600

    contact stylized.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -28,24 +28,26 @@ class Contact extends Component{
     return (
       <div className = "Contact" id="Contact">
         <SectionHeader title="Contact"/>
-          <div className="h3">Lets Work Together!</div>
-          <div className="h4">Leave your name and email with a message and I will get back to you.</div>
-        <div className="content">
-          <div className="name h3">
-            <label htmlFor="">Name</label>
-            <input type="text"  title="name" onChange={this.handleInput}/>
+        <div className="container">
+          <div className="h3 intro">Lets Work Together!</div>
+            <div className="h4 direction">Leave your name and email with a message and I will get back to you.</div>
+          <div className="content">
+            <div className="name h3">
+              <label htmlFor="">Name</label>
+              <input type="text"  title="name" onChange={this.handleInput}/>
+            </div>
+            <div className="email h3">
+              <label htmlFor="">Email</label>
+              <input type="text"  title="email" onChange={this.handleInput}/>
+            </div>
+            <div className="message h3">
+              <label htmlFor="">Message</label>
+              <textarea className="message"  title="message" name="message" rows="10"  onChange={this.handleInput}></textarea>
+            </div>
+            <button className="h3 send" onClick={this.sendEmail}>Send Email</button>
           </div>
-          <div className="email h3">
-            <label htmlFor="">Email</label>
-            <input type="text"  title="email" onChange={this.handleInput}/>
-          </div>
-          <div className="message h3">
-            <label htmlFor="">Message</label>
-            <textarea className="message"  title="message" name="message" rows="10"  onChange={this.handleInput}></textarea>
-          </div>
-          <button className="h3" onClick={this.sendEmail}>Send Email</button>
-
         </div>
+
       </div>
     )
   }
