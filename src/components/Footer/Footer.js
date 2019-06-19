8fe63bf0311309bab29d426d3c69d66890a426d4
Alan Curtis <acurtis@badgersport.local>
2019-06-18 23:07:58 -0600

    added social media icons and links in footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,10 @@ const Footer = () => {
   // TODO: Links to my linked in, art Station, github, maybe facebook.
   
   return (
-    <div className = "Footer">
-      Footer comp
-      <button>Back to Top </button>
+    <div className="Footer">
+      <a className="h4" href="https://www.linkedin.com/in/alan-curtis-7aa94bb/" target="_blank" rel="noopener noreferrer"><img src=".\images\socialMedia\linkedIn.png" alt="linkedIn"/></a>
+      <a className="h4" href="https://github.com/TalanCurtis" target="_blank" rel="noopener noreferrer"><img src=".\images\socialMedia\gitHub.png" alt="gitHub"/></a>
+      <a className="h4" href="https://www.artstation.com/alancurtis31" target="_blank" rel="noopener noreferrer"><img src=".\images\socialMedia\artStation.png" alt="artStation"/></a>
     </div>
   )
 }
