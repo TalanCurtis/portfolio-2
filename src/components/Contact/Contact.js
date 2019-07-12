e7ddac445cec7b3d8392ea72f6b4b0b4bd4b3dad
Alan Curtis <acurtis@badgersport.local>
2019-07-12 01:07:32 -0600

    animated mail. disabled button.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import SectionHeader from '../SectionHeader/SectionHeader';
+import EmailSvg from '../../images/EmailSvg';
+import { Power4 , TimelineMax } from 'gsap';
 
 class Contact extends Component{
   constructor(props){
@@ -10,24 +12,49 @@ class Contact extends Component{
       email:"",
       message:""
     };
+    this.tl = new TimelineMax({paused: true});
   }
 
- // TODO: after send mail should get a confirmation or pop up alert saying it was sent.
- // TODO: disable send message until name, valid email, and message has input.
+ componentDidMount(){
+   this.animation();
+ }
+
+  animation = () => {
+    this.tl.set("#envelope-all", {opacity: 0 , zIndex: 600000});
+    this.tl.to("#envelope-all", .4, {opacity: 1 , ease: Power4.easeInOut });
+    this.tl.set('#lid-front', {opacity: 0, transformOrigin:"50% 96%"});
+    this.tl.to("#letter", 1, {  y: "204%", ease: Power4.easeInOut });
+    this.tl.set('#lid-front', {opacity: 1});
+    this.tl.set('#lid-back', {opacity: 0});
+    this.tl.to("#lid-front", .5, { scaleY:-1 ,  ease: Power4.easeOut });
+    this.tl.to("#envelope-all", .3, {x:"-3%" , ease: Power4.easeInOut });
+    this.tl.to("#envelope-all", 1, {x:"+200%", opacity: 0, ease: Power4.easeOut });
+  }
 
   sendEmail = () => {
-    console.log('hello',)
     const {name, email, message} = this.state
-    axios.post('/api/email', {name, email, message}).then((res)=>console.log("response: ", res.data)).catch((err)=>console.log('error:',err))
+    this.tl.restart();
+    axios.post('/api/email', {name, email, message}).then((res)=>console.log("response: ", res.data)).catch((err)=>console.log('error:',err));
+    this.setState({ 
+      name:"",
+      email:"",
+      message:""
+    })
   }
 
   handleInput = (event) => {
     this.setState({
       [event.target.title]: event.target.value,
     })
-    
   }
+
   render(){
+    let isDisabled = true;
+    let disabled = "disabled";
+    if (this.state.name && this.state.email && this.state.message ){
+      isDisabled = false;
+      disabled = "";
+    }
     return (
       <div className = "Contact" id="Contact">
         <SectionHeader title="Contact"/>
@@ -37,17 +64,18 @@ class Contact extends Component{
           <div className="content">
             <div className="name h3">
               <label htmlFor="">Name</label>
-              <input type="text"  title="name" onChange={this.handleInput}/>
+              <input type="text"  title="name" onChange={this.handleInput} value={this.state.name}/>
             </div>
             <div className="email h3">
               <label htmlFor="">Email</label>
-              <input type="text"  title="email" onChange={this.handleInput}/>
+              <input type="text"  title="email" onChange={this.handleInput} value={this.state.email}/>
             </div>
             <div className="message h3">
               <label htmlFor="">Message</label>
-              <textarea className="message"  title="message" name="message" rows="10"  onChange={this.handleInput}></textarea>
+              <textarea className="message"  title="message" name="message" rows="10"  onChange={this.handleInput} value={this.state.message}></textarea>
             </div>
-            <button className="h3 send colorBtn" onClick={this.sendEmail}>Send Email</button>
+            <button disabled={isDisabled} className={`h3 send colorBtn ${disabled}`} onClick={this.sendEmail}>Send Email</button>
+            <EmailSvg size={200} />
           </div>
         </div>
 
